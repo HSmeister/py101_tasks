7a22a7ed2a0930202443fa85d062c41747317b52
HSmeister <germannene15@gmail.com>
2020-11-07 18:49:44 +0100

    password.py done

--- password.py
+++ password.py
@@ -5,6 +5,20 @@
 Сложным считается пароль, состоящий как минимум из 8-ми символов,
 включая цифры и алфавитные символы
 """
+count = 0
+password = input('Введите ваш пароль:')
+for char in password:
+    count += 1
+    if char.isupper:
+        upper_check = True
+    if char.islower:
+        lower_check = True
+    if char.isdigit:
+        digit_check = True
+if count >= 8 and upper_check and lower_check and digit_check:
+    print('Strong enough')
+else:
+    print('Too weak')
 
 if __name__ == '__main__':
     pass
