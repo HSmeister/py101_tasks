5d28d6d51231d0cf64b7f84e4d7bfb34df344343
HSmeister <germannene15@gmail.com>
2020-11-07 21:37:12 +0100

    mb done

--- word_freq.py
+++ word_freq.py
@@ -10,3 +10,25 @@
 
 if __name__ == '__main__':
     pass
+
+import argparse
+import string
+import nltk
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+parser = argparse.ArgumentParser(description='Toп-100 слов')
+parser.add_argument('file', type=str, help='Путь к файлу')
+args = parser.parse_args()
+print(args.file)
+text_file = open(args.file, 'r')
+print(text_file.read())
+stopwords = set(nltk.corpus.stopwords.words('english'))
+tokens = nltk.word_tokenize(text_file)
+tokens = [word for word in tokens if (word not in string.punctuation)]
+for bad_word in stopwords:
+        while bad_word in tokens:
+            tokens.remove(bad_word)
+word_freq = Counter(tokenized_words)
+print(word_freq.most_common(args_values.count))
